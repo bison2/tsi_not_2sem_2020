ea0a2b732d9187d7ebb356f23f3ba18ae22fda13
bison2 <profvaldes@gmail.com>
2020-12-04 11:19:07 -0200

    escondendo a chave

--- loja1/loja1/settings.py
+++ loja1/loja1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b#2v2#+dzq43(g#)$g0@tr@)32_j0wymx2pu_4w6(4l#sy(@3h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
