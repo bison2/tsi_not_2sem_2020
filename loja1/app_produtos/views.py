a6edd745c9fe59f6d858c69ebbb54bba7fe65a82
bison2 <profvaldes@gmail.com>
2020-12-08 09:37:05 -0200

    criando a classe detail do produto, e a funcao detail do produto na views.py . Coloquei as urls deles no urls.py, e criei o detail.html

--- loja1/app_produtos/views.py
+++ loja1/app_produtos/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -28,15 +28,22 @@ class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = "app_produtos/detail.html"
     
-    #def get_context_data(self, *args, **kwargs):
-        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
 
 #Function Based View
-def product_detail_view(request):
-    queryset = Product.objects.all()
+def product_detail_view(request, pk=None, *args, **kwargs):
+    #print(args)
+    #print(kwargs)
+    #instance = Product.objects.get(pk = pk) #get the object id
+    instance = get_object_or_404(Product, pk = pk)
     context = {
-        'object_list': queryset
+        'object': instance
     }
+    #queryset = Product.objects.all()
+    #context = {
+     #   'object_list': queryset
+    #}
     return render(request, "app_produtos/detail.html", context)
