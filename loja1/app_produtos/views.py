bbcf1f3eff31f308c95e3e994967e135a1cddf3a
bison2 <profvaldes@gmail.com>
2020-12-09 11:06:34 -0200

    customizando as query-sets, criei na views.py da app_produtos a classe e a funcao featured, acrescentei as novas urls, criei no templates o arq. featured-detail.html, fiz as migracoes

--- loja1/app_produtos/views.py
+++ loja1/app_produtos/views.py
@@ -5,17 +5,23 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+class ProductFeaturedListView(ListView):
+    template_name = "app_produtos/list.html"
+    
+    def get_queryset(self, *args, **kwargs):
+        return Product.objects.featured()
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "app_produtos/featured-detail.html"
+
 #Class Based View
-class ProductListView(ListView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
+class ProductListView(ListView):    
+#traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "app_produtos/list.html"
     
-    #def get_context_data(self):
-        #context = super(ProductListView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
-
+    
 #Function Based View
 def product_list_view(request):
     queryset = Product.objects.all()
@@ -45,25 +51,7 @@ class ProductDetailView(DetailView):
 
 #Function Based View
 def product_detail_view(request, pk=None, *args, **kwargs):
-    #print(args)
-    #print(kwargs)
-    #instance = Product.objects.get(pk = pk) #get the object id
-    #instance = get_object_or_404(Product, pk = pk)
-    #
-    # try:
-     #   instance = Product.objects.get(id = pk)
-    #except Product.DoesNotExist:
-        #print("Nenhum produto encontrado aqui!")
-        #raise Http404('Esse produto não existe!')
-
-    #qs = Product.objects.filter(id = pk)
-    #if qs.exists():
     
-    #if qs.count() == 1:
-     #   instance = qs.first()
-    #else:
-     #   raise Http404("Esse produto não existe!")
-
     instance = Product.objects.get_by_id(pk)
     print(instance)
     if instance is None:
@@ -72,8 +60,5 @@ def product_detail_view(request, pk=None, *args, **kwargs):
     context = {
         'object': instance
     }
-    #queryset = Product.objects.all()
-    #context = {
-     #   'object_list': queryset
-    #}
+    
     return render(request, "app_produtos/detail.html", context)
