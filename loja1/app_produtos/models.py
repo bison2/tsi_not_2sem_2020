30d273e862d9b7af9e832d01a9f04069dac6b6ac
bison2 <profvaldes@gmail.com>
2020-12-09 09:23:44 -0200

    criando a classe manager do produto na models.py da app_produtos. Essa classe especifica gerencia e possibilita fazer consultas mais elaboradas do que a classe manager objects.

--- loja1/app_produtos/models.py
+++ loja1/app_produtos/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id = id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+
+
 # Create your models here.
 class Product(models.Model): #product_category
     title = models.CharField(max_length=120)
@@ -7,6 +16,8 @@ class Product(models.Model): #product_category
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.ImageField(upload_to = 'products/', null = True, blank = True)
 
+    objects = ProductManager()
+    
     #python 3
     def __str__(self):
         return self.title
