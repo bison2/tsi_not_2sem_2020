48b1b55b50a160ec6e49c0bc71d5bf32baca42b4
bison2 <profvaldes@gmail.com>
2020-12-09 09:36:53 -0200

    mudanca na app_produtos/models.py , a classe ProductManager foi criada e instanciada. Mudanca na app_produtos/views.py na classe CBV e na FBV

--- loja1/app_produtos/views.py
+++ loja1/app_produtos/views.py
@@ -27,13 +27,21 @@ def product_list_view(request):
 #Class Based View
 class ProductDetailView(DetailView):
     #traz todos os produtos do banco de dados sem filtrar nada 
-    queryset = Product.objects.all()
+    
+    #queryset = Product.objects.all()
     template_name = "app_produtos/detail.html"
     
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
 
 #Function Based View
 def product_detail_view(request, pk=None, *args, **kwargs):
@@ -48,11 +56,17 @@ def product_detail_view(request, pk=None, *args, **kwargs):
         #print("Nenhum produto encontrado aqui!")
         #raise Http404('Esse produto não existe!')
 
-    qs = Product.objects.filter(id = pk)
+    #qs = Product.objects.filter(id = pk)
     #if qs.exists():
-    if qs.count() == 1:
-        instance = qs.first()
-    else:
+    
+    #if qs.count() == 1:
+     #   instance = qs.first()
+    #else:
+     #   raise Http404("Esse produto não existe!")
+
+    instance = Product.objects.get_by_id(pk)
+    print(instance)
+    if instance is None:
         raise Http404("Esse produto não existe!")
 
     context = {
