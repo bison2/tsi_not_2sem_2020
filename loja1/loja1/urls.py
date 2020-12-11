5095e14ef5c16774e41d6720eefe5e117848ae76
bison2 <profvaldes@gmail.com>
2020-12-10 22:24:48 -0200

    criei o urls.py na app_produtos. Mudei o urls.py da loja1. Mudei o models.py para usar a funcao get_absolute_url. Ficou apenas a class based view(CBV),  a fbv foi retirada. o templates/list.html foi colocado um href , q faz os produtos serem acessados num clique , e esconde o id do produto da url.

--- loja1/loja1/urls.py
+++ loja1/loja1/urls.py
@@ -1,17 +1,16 @@
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from app_produtos.views import (ProductListView,
-                                 product_list_view,
-                                 ProductDetailView,
-                                 product_detail_view,
-                                 ProductFeaturedListView,
-                                 ProductFeaturedDetailView)
-
-from .views import home_page, about_page, contact_page, login_page, register_page
+from .views import (home_page, 
+                    about_page, 
+                    contact_page, 
+                    login_page, 
+                    register_page
+)
 
 urlpatterns = [
     path('', home_page),
@@ -19,15 +18,11 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
-    path('featured/', ProductFeaturedListView.as_view()),
-    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
-    path('products/', ProductListView.as_view()),
-    path('products-fbv/', product_list_view),
-    path('products/<int:pk>', ProductDetailView.as_view()),
-    path('products-fbv/<int:pk>', product_detail_view),     
+    path('products/', include("app_produtos.urls")),
     path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
