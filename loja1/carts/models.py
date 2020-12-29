4700bc8673e5597543c3eff2f2c09257413aec19
bison2 <profvaldes@gmail.com>
2020-12-28 23:18:16 -0200

    criei o total do carrinho de compras.Mudancas no models.py.

--- loja1/carts/models.py
+++ loja1/carts/models.py
@@ -1,6 +1,7 @@
+
 from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save, m2m_changed
 from app_produtos.models import Product
 
 User = settings.AUTH_USER_MODEL
@@ -20,7 +21,6 @@ class CartManager(models.Manager):
             new_obj = True
             request.session['cart_id'] = cart_obj.id
         return cart_obj, new_obj
-
     def new(self, user = None):
         user_obj = None
         if user is not None:
@@ -28,11 +28,11 @@ class CartManager(models.Manager):
                 user_obj = user
         return self.model.objects.create(user = user_obj)
 
-
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
     products = models.ManyToManyField(Product, blank = True)
     total = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
+    subtotal = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
     updated = models.DateTimeField(auto_now = True)
     timestamp = models.DateTimeField(auto_now_add = True)
 
@@ -40,3 +40,26 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+  #print(action)
+  if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+    #print(instance.products.all())
+    #print(instance.total)
+    products = instance.products.all() 
+    total = 0 
+    for product in products: 
+      total += product.price 
+    if instance.subtotal != total:
+      instance.subtotal = total
+      instance.save()
+    #print(total) 
+    instance.subtotal = total
+    instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender = Cart.products.through)
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+  instance.total = instance.subtotal + 10
+
+pre_save.connect(pre_save_cart_receiver, sender = Cart)
--- loja1/carts/views.py
+++ loja1/carts/views.py
@@ -3,5 +3,6 @@ from .models import Cart
 
 def cart_home(request):
     cart_obj = Cart.objects.new_or_get(request) 
-    return render(request, "carts/home.html", {} )
+    
+    return render(request, "carts/home.html", {})
     
